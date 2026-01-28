c3071616f9d296c5a20bc6539670650af467414c
Calvin Loveland <calvinloveland@gmail.com>
2026-01-28 21:31:06 +0000

    powder_play: load sand and water in test

--- active/games/powder_play/frontend/e2e/playwright/discovered_sand_water.spec.ts
+++ active/games/powder_play/frontend/e2e/playwright/discovered_sand_water.spec.ts
@@ -20,6 +20,9 @@ test('sand + water mix appears in discovered list', async ({ page }) => {
   await page.goto('http://127.0.0.1:5173/');
   await page.waitForSelector('text=Alchemist Powder');
 
+  await page.click('#materials-list >> text=Sand');
+  await page.click('#materials-list >> text=Water');
+
   await page.waitForFunction(() => {
     const map = (window as any).__materialIdByName || {};
     return !!(window as any).__powderWorker && !!map.Sand && !!map.Water;
